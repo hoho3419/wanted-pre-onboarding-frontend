0009a6863e2a0da6c7752642bd33d8ad33eef7c5
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-07-31 16:01:15 +0900

    Router 및 레이아웃 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home,Login,Register,Todos } from './pages';
+import { styled } from 'styled-components';
+import { Header } from './components'
+import { PrivateRoute,ProtectedRoute } from './route';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header/>
+      <Routes >
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signin' element={<ProtectedRoute component={<Login/>}/> }/>
+        <Route path='/signup' element={<ProtectedRoute component={<Register/>}/> }/>
+        <Route path='/todo' element={<PrivateRoute  component={<Todos/>}/> }/>
+      </Routes>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  height: 100vh;
+  margin: auto;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
